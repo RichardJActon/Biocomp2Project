b4a6fa104d325e485ef17d1015f15db16ac94960
GabrieleNocchi <g.nocchi@hotmail.com>
2016-03-05 14:03:31 +0000

    Codon Frequency Calculation
    No code changes; comments only.

--- middlelayer/codonsusage.pl
+++ middlelayer/codonsusage.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 # It starts by creating an array with all codons of a sequence;
-# I will need a string with the sequence from the sql query of an entry.
+# I will need a string with the extracted and joined coding sequence for an entry.
 
 my $nucleo_seq = <>;
 my $aa_seq = <>;
@@ -14,7 +14,7 @@ my @aminos = $aa_seq =~ /[A-Z]/gi;
 
 
 # Below I calculate the frequency of each codon within the coding regions of an entry;
-# To do this I need to count how many of each codon there are; I do it by iterating
+# To do this I need to count how many of each codon there are in the coding sequence; I do it by iterating
 # my codon array and counting each codon with an hash;
 # Then I use the total number of codon, which is the size of my initial codon array, to
 # calculate the percentage of each codon.
@@ -23,10 +23,10 @@ my @aminos = $aa_seq =~ /[A-Z]/gi;
 ##### THIS MECHANISM CAN ALSO BE USED TO CALCULATE THE CODON FREQUENCY IN THE WHOLE CHROMOSOME ####
 ##### TO DO THAT I WILL NEED AN SQL QUERY WHICH PASS ME ALL SEQUENCES IN AN ARRAY WHERE ####
 ##### EACH VALUES IS A SEQUENCE OF THE CHROMOSOME####
-##### THE ARRAY; THAT WAY I CAN ITERATE THROUGH THE ARRAY AND CONCATENATE EACH CODING SEQUENCE AS A VERY LONG STRING ####
-##### THEN I CAN TREAT IT THE SAME WAY AS I TRATED A SMALL SEQUENCE BY DIVIDING IT IN CODONS ####
+##### THAT WAY I CAN ITERATE THROUGH THE ARRAY AND CONCATENATE EACH CODING SEQUENCE AS A VERY LONG STRING ####
+##### THEN I CAN TREAT IT THE SAME WAY AS A SMALL SEQUENCE BY DIVIDING IT IN CODONS AND ####
 ##### MAKING AN HASH WITH EACH CODON COUNT AND THEN CALCULATE THE FREQUENCY USING THE TOTAL NUMBER OF CODONS ####
-#### IN THE CHROMOSOME.
+#### IN THE CHROMOSOME.####
 
 my %codon_count;
 
