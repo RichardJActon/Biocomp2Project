7310d9faf89a9c4725e93eb029f2549238a3b1e5
RichardJActon <rjactonspsfcf@gmail.com>
2016-03-08 22:22:27 +0000

    updates to database code and API spreadsheet

--- database/Chromosome17.sql
+++ database/Chromosome17.sql
@@ -1,13 +1,14 @@
 -- 
-DROP DATABASE Chromosome17;
+DROP DATABASE IF EXISTS Chromosome17;
 CREATE DATABASE Chromosome17;
 USE Chromosome17;
 -- 
+
 CREATE TABLE Chromosome_Locations
 (	Chromosome_Location_ID	SMALLINT 	DEFAULT NULL,
 	Location_Name			TEXT		NOT NULL,
 	PRIMARY KEY (Chromosome_Location_ID)
-);
+)TYPE = INNODB;
 
 CREATE TABLE Loci
 (	Genebank_Accession		VARCHAR(8)	DEFAULT NULL,
@@ -18,8 +19,7 @@ CREATE TABLE Loci
 	CDS_translated 			TEXT		NOT NULL,
 	CDS_untranslated		TEXT		NOT NULL,
 	PRIMARY KEY (Genebank_Accession)-- ,
---  	FOREIGN KEY (Chromosome_Location_ID) REFERENCES Chromosome_Locations(Chromosome_Location_ID)
-);
+)TYPE = INNODB;
 
 CREATE TABLE Exons
 (	Exon_ID				SMALLINT	DEFAULT NULL,
@@ -27,5 +27,17 @@ CREATE TABLE Exons
 	StartPosition		SMALLINT	NOT NULL,
 	EndPosition			SMALLINT	NOT NULL,
 	PRIMARY KEY (Exon_ID)-- ,
--- 	FOREIGN KEY (Genebank_Accession) REFERENCES Loci(Genebank_Accession)
-);
+)TYPE = INNODB;
+#############################
+#### NOT NULLS? foreign key problems>?
+
+ALTER TABLE Exons ADD CONSTRAINT exon_loci_Accession
+FOREIGN KEY (Genebank_Accession) REFERENCES Loci (Genebank_Accession) 
+ON UPDATE CASCADE ON DELETE CASCADE;
+--
+ALTER TABLE Loci ADD CONSTRAINT loci_chromLoc_ChromLocID
+FOREIGN KEY (Chromosome_Location_ID) REFERENCES Chromosome_Locations (Chromosome_Location_ID) 
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+##load - > index
