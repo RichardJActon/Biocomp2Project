5860aa26c46e9736cbbfabc74d97504dfaa962b9
RichardJActon <rjactonspsfcf@gmail.com>
2016-03-08 13:30:15 +0000

    tidying parser code

--- database/parser.pl
+++ database/parser.pl
@@ -50,12 +50,10 @@ for (my $i = 0; $i < scalar @loci; $i++)
 					$subline = "";
 				}
 				$subline =~ s/[0-9]|\n|\s//g;
-				#print "$subline\n";
 				$seq .= $subline;
 				$subline = "";
 			}
 			push @seqs, $seq;
-			#print "$seq\n";
 			$seq = "";
 		}
 	}
@@ -64,42 +62,3 @@ for (my $i = 0; $i < scalar @loci; $i++)
 for (my $i = 0; $i < scalar @seqs; $i++) {
 	print "$seqs[$i]\n";
 }
-#print "$seqs[0]\n\n";
-
-########################################################
-# my %attributes;
-# my $attribute = "";
-# my $subline = "";
-# 	for (my $i = 0; $i < scalar @loci; $i++) 
-# 	{
-# 		my $j = 0;
-# 		print $lines[$j];
-# 		until ($lines[$j] =~ /^LOCUS\s{7}$loci[$i]/) 
-# 		{
-# 			print $lines[$j];
-# 			$j++;
-# 			until ($lines[$j] =~ /^ORIGIN/)
-# 			{
-# 				print $lines[$j];
-# 				$j++;
-# 				$subline = $lines[$j];
-# 				$subline =~ s/[0-9]|\n//g;
-# 				$attribute = $attribute . $subline;
-# 				if ($lines[$j] = /\/\//) 
-# 				{
-# 					last;
-# 				}
-# 			}
-# 		}
-# 	$attributes{$loci[$i]} = $attribute;
-# 	$attribute = "";
-# 	$subline = "";
-#	}
-
-#%seqs = DBsubroutines::EXTRACT_LOCUS_FEATURE(@loci,@lines);
-
-
-# print %seqs;
-# print %attributes;
-
-
