b87ffa033a906afb081c0e49f5a0f953e989e28c
GabrieleNocchi <g.nocchi@hotmail.com>
2016-03-10 00:43:10 +0000

    Created module for first query
    Added first subroutine to retrieve an array of accession numbers.

---
+++ middlelayer/firstquery.pm
@@ -0,0 +1,33 @@
+package middle::firstquery;
+
+# Subroutine: get_accession
+# Purpose: executes user search and retrieve accession number/s from the database.
+# Input paramater: 2 strings; the 1st string is the type of search (product 
+# name, genbank_accession, gene_id or location) captured from the dropdown
+# box of the application; the second string is what the user types in the searchbox of the application.
+# Returns: an array containing 1 or more accession numbers (if the search type
+# is location the search could return more than 1 accession number).
+
+sub get_accession
+
+{
+   chomp $_[1];
+
+   my $sql = "SELECT Genebank_Accession FROM Loci WHERE $_[0] = '$_[1]'";
+
+   my $dbh = DBI->connect($dbsource, $username, $password);
+
+
+   my $sth = $dbh->prepare($sql);
+
+   my @array;
+
+   if($sth && $sth->execute)   {
+        
+      while(my ($name ) = $sth->fetchrow_array)   {
+         push @array, $name;
+      }
+      return @array;
+   }
+}
+
