1858ae44160798063da09526f4f0c06b067980fe
DolapoA <dolaajayi@hotmail.com>
2016-03-13 16:35:58 +0000

    Added files via upload

--- frontend/proj(cgi).pl
+++ frontend/proj(cgi).pl
@@ -1,13 +1,56 @@
 #!/usr/bin/perl
 use CGI;
 use strict;
+use middle::firstquery;
 my $cgi = new CGI;
 print $cgi->header();
 
+#This section uses the firstquery module and displays what is returned from the module in a results html page.
+
+
+#The following are the 2 strings captured from the user
+
+#This is the type of string chosen by the user
+my $Search_type = $cgi->param('Search_type');
+
+#This is the actual string inputed by the user
+my $User_input = $cgi->param('$User_input');
+	 
+
+
 print <<__EOF;
 
->>Insert list of genes and their respective Gene Identifiers, Protein product names and Genbank accession
+<html>
+<head>
+<style type='text/css'>
 
+<!--
+body { background: #;
+       color: ; }
+h1   { color: ;
+	font-family: calibri; 
+	font-size: 100%;  }
+-->
+<title>Search result(s)</title>
+</head>	
+
+</style>
+</html>
 
 __EOF
+
+#This line calls on a function in the firstquery.pm module called the get_accessions subroutine
+#which processes the 2 inputs of the user.
+
+my @Accessions = get_accession($Search_type, $User_input)
+
+
+
+
+foreach my $key (keys %OrderedAccessions){
+	print "<p>$key = $OrderedAccessions{$key}</p>";
+}
+
+
+
 
