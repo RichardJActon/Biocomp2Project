d299d151ba37062440dfdb8d7f0463fa48fb2855
RichardJActon <rjactonspsfcf@gmail.com>
2016-03-19 21:48:49 +0000

    subroutine to carry out query returning an accession number when given a locus gi - beggings of subroutine to return Exon positions when given an accession

--- database/test1.pl
+++ database/test1.pl
@@ -1,22 +1,36 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use DBsubroutines;
+#use DBsubroutines;
+use Queries;
 use DBI;
 ################################################################################
 
+my $searchString = "34424";
+my $result = Queries::LOCUS_GI_SEARCH($searchString);
+print "$result\n";
+################################################################################
 
+my $searchStringx = 'Y16787';
+my ($start,$end) = Queries::EXON_POSITIONS($searchStringx);
+my (@starts,@ends) = (@{$start},@{$end});
+
+for (my $i = 0; $i < scalar @starts; $i++) 
+{
+	print "$starts[$i]\n";
+	print "$ends[$i]\n";
+}
 
 
 ################################################################################
-my $dbname = "Chromosome17";
-my $dbserver = "localhost";
-my $dbport = "3306";
-my $datasource = "dbi:mysql:database=$dbname;host=$dbserver";
-my $username = "RichardActon";
-my $password = "314159";
+# my $dbname = "Chromosome17";
+# my $dbserver = "localhost";
+# my $dbport = "3306";
+# my $datasource = "dbi:mysql:database=$dbname;host=$dbserver";
+# my $username = "RichardActon";
+# my $password = "314159";
 
-my $dbh = DBI->connect($datasource, $username, $password);
+# my $dbh = DBI->connect($datasource, $username, $password);
 ################################################################################
 # my $dbname = "ar001";
 # my $dbserver = "localhost";
@@ -27,13 +41,13 @@ my $dbh = DBI->connect($datasource, $username, $password);
 
 # my $dbh = DBI->connect($datasource, $username, $password);
 ################################################################################
-for (my $i = 0; $i < scalar @loci; $i++) {
-	my $sql = ("
-		INSERT INTO Loci (Genebank_Accession)
-			VALUES ('$loci[$i]');
-		");
-	$dbh->do($sql);
-};
+# for (my $i = 0; $i < scalar @loci; $i++) {
+# 	my $sql = ("
+# 		INSERT INTO Loci (Genebank_Accession)
+# 			VALUES ('$loci[$i]');
+# 		");
+# 	$dbh->do($sql);
+# };
 
 
 
