7f3ae0208cd477134ad47278fb6c31926fcd77b8
GabrieleNocchi <g.nocchi@hotmail.com>
2016-03-29 01:58:41 +0100

    Missing tag

--- frontend/projcgi.pl
+++ frontend/projcgi.pl
@@ -77,7 +77,7 @@ if (get_results($search_type, $user_input))   {
 else   {
 
 
-    print "<p> Your search did not find any result. <p>";
+    print "<p> Your search did not find any result. </p>";
 
 }
 
