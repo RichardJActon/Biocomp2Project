e03338eed1c37fe048c330174e99e53177d4a85a
RichardJActon <rjactonspsfcf@gmail.com>
2016-04-04 16:14:02 +0100

    fixed misreading that put protien sequence in incorrect feild

--- database/database_importer.sql
+++ database/database_importer.sql
@@ -1,9 +1,9 @@
--- import "|" separated file into database
+﻿-- import "|" separated file into database
 -- 
 LOAD DATA LOCAL INFILE 'loci.txt' INTO TABLE Loci
 FIELDS TERMINATED BY '|'
 LINES TERMINATED BY '\n'
-(Genbank_Accession,Locus_GI,DNA_seq,Product_Name,CDS_untranslated,Reading_frame);
+(Genbank_Accession,Locus_GI,DNA_seq,Product_Name,CDS_translated,Reading_frame);
 -- 
 LOAD DATA LOCAL INFILE 'chromloc.txt' INTO TABLE Chromosome_Locations
 FIELDS TERMINATED BY '|'
@@ -15,9 +15,3 @@ FIELDS TERMINATED BY '|'
 LINES TERMINATED BY '\n'
 (Genbank_Accession, StartPosition,EndPosition,Exon_ID);
 -- indices?
--- ALTER TABLE Loci INDEX(Genbank_Accession,Locus_GI,Product_Name);
--- ALTER TABLE Chromosome_Locations INDEX(Genbank_Accession,Location_Name);
--- ALTER TABLE Exons INDEX(Genbank_Accession,Exon_ID);
-
--- NB use "mysql --local-infile -u..." when running this script 
--- on newer versions of SQL which do not like the LOCAL comand
