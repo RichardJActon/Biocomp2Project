4a9529329de8b8dfa815a56a8b410ade0329ea43
GabrieleNocchi <g.nocchi@hotmail.com>
2016-04-05 15:41:22 +0100

    Added static table with Chromosome freq

--- frontend/projcgi2.pl
+++ frontend/projcgi2.pl
@@ -194,6 +194,90 @@ foreach my $codon (sort keys %codon_freq){
 __EOF
 }
 
+
+
+print <<__EOF;
+</table>
+<br / >
+
+<h2>Codon usage information for Chromosome 17: </h2>
+<br / >
+
+<div style="overflow-x:auto;">
+<table border ="2">
+<tr >
+<td><h3> Amino Acid </h3></td>
+<td><h3> Codon </h3></td>
+<td><h3> Codon Frequency %</h3> </td>
+<td><h3> Codon Ratio %</h3></td>
+</tr>
+<tr><td>Lysine</td><td>aaa</td><td>1.59</td><td>45.91</td></tr>
+<tr><td>Aspargine</td><td>aac</td><td>1.18</td><td>54.32</td></tr>
+<tr><td>Lysine</td><td>aag</td><td>1.88</td><td>54.09</td></tr>
+<tr><td>Aspargine</td><td>aat</td><td>1.00</td><td>45.68</td></tr>
+<tr><td>Tryptophan</td><td>aca</td><td>1.47</td><td>18.05</td></tr>
+<tr><td>Tryptophan</td><td>acc</td><td>1.83</td><td>22.53</td></tr>
+<tr><td>Tryptophan</td><td>acg</td><td>0.80</td><td>9.83</td></tr>
+<tr><td>Tryptophan</td><td>act</td><td>1.22</td><td>15.04</td></tr>
+<tr><td>Arginine</td><td>aga</td><td>2.17</td><td>25.67</td></tr>
+<tr><td>Serine</td><td>agc</td><td>2.38</td><td>24.72</td></tr>
+<tr><td>Arginine</td><td>agg</td><td>2.45</td><td>28.93</td></tr>
+<tr><td>Serine</td><td>agt</td><td>1.08</td><td>11.27</td></tr>
+<tr><td>Isoleucine</td><td>ata</td><td>0.57</td><td>22.08</td></tr>
+<tr><td>Isoleucine</td><td>atc</td><td>1.06</td><td>41.55</td></tr>
+<tr><td>Methionine</td><td>atg</td><td>1.50</td><td>100.00</td></tr>
+<tr><td>Isoleucine</td><td>att</td><td>0.93</td><td>36.37</td></tr>
+<tr><td>Glutamine</td><td>caa</td><td>1.65</td><td>38.56</td></tr>
+<tr><td>Histidine</td><td>cac</td><td>1.50</td><td>56.20</td></tr>
+<tr><td>Glutamine</td><td>cag</td><td>2.62</td><td>61.44</td></tr>
+<tr><td>Histidine</td><td>cat</td><td>1.17</td><td>43.80</td></tr>
+<tr><td>Proline</td><td>cca</td><td>2.57</td><td>27.28</td></tr>
+<tr><td>Proline</td><td>ccc</td><td>2.76</td><td>29.27</td></tr>
+<tr><td>Proline</td><td>ccg</td><td>1.33</td><td>14.08</td></tr>
+<tr><td>Proline</td><td>cct</td><td>2.77</td><td>29.37</td></tr>
+<tr><td>Arginine</td><td>cga</td><td>0.73</td><td>8.68</td></tr>
+<tr><td>Arginine</td><td>cgc</td><td>1.19</td><td>14.10</td></tr>
+<tr><td>Arginine</td><td>cgg</td><td>1.28</td><td>15.07</td></tr>
+<tr><td>Arginine</td><td>cgt</td><td>0.64</td><td>7.55</td></tr>
+<tr><td>Leucine</td><td>cta</td><td>0.83</td><td>9.17</td></tr>
+<tr><td>Leucine</td><td>ctc</td><td>2.02</td><td>22.41</td></tr>
+<tr><td>Leucine</td><td>ctg</td><td>2.92</td><td>32.42</td></tr>
+<tr><td>Leucine</td><td>ctt</td><td>1.45</td><td>16.06</td></tr>
+<tr><td>Glutamic Acid</td><td>gaa</td><td>1.83</td><td>44.16</td></tr>
+<tr><td>Aspartic Acid</td><td>gac</td><td>1.38</td><td>53.67</td></tr>
+<tr><td>Glutamic Acid</td><td>gag</td><td>2.31</td><td>55.84</td></tr>
+<tr><td>Aspartic Acid</td><td>gat</td><td>1.19</td><td>46.33</td></tr>
+<tr><td>Alanine</td><td>gca</td><td>1.95</td><td>24.05</td></tr>
+<tr><td>Alanine</td><td>gcc</td><td>2.64</td><td>32.65</td></tr>
+<tr><td>Alanine</td><td>gcg</td><td>1.18</td><td>14.61</td></tr>
+<tr><td>Alanine</td><td>gct</td><td>2.32</td><td>28.69</td></tr>
+<tr><td>Glycine</td><td>gga</td><td>2.54</td><td>29.85</td></tr>
+<tr><td>Glycine</td><td>ggc</td><td>2.31</td><td>27.18</td></tr>
+<tr><td>Glycine</td><td>ggg</td><td>2.35</td><td>27.62</td></tr>
+<tr><td>Glycine</td><td>ggt</td><td>1.31</td><td>15.35</td></tr>
+<tr><td>Valine</td><td>gta</td><td>0.64</td><td>14.21</td></tr>
+<tr><td>Valine</td><td>gtc</td><td>1.09</td><td>24.37</td></tr>
+<tr><td>Valine</td><td>gtg</td><td>1.91</td><td>42.64</td></tr>
+<tr><td>Valine</td><td>gtt</td><td>0.84</td><td>18.79</td></tr>
+<tr><td>Stop</td><td>taa</td><td>0.68</td><td>21.78</td></tr>
+<tr><td>Tyrosine</td><td>tac</td><td>0.77</td><td>51.24</td></tr>
+<tr><td>Stop</td><td>tag</td><td>0.56</td><td>17.79</td></tr>
+<tr><td>Tyrosine</td><td>tat</td><td>0.73</td><td>48.76</td></tr>
+<tr><td>Serine</td><td>tca</td><td>1.54</td><td>15.98</td></tr>
+<tr><td>Serine</td><td>tcc</td><td>2.33</td><td>24.26</td></tr>
+<tr><td>Serine</td><td>tcg</td><td>0.71</td><td>7.37</td></tr>
+<tr><td>Serine</td><td>tct</td><td>1.58</td><td>16.40</td></tr>
+<tr><td>Stop</td><td>tga</td><td>1.89</td><td>60.44</td></tr>
+<tr><td>Cysteine</td><td>tgc</td><td>1.98</td><td>60.63</td></tr>
+<tr><td>Tryptophan</td><td>tgg</td><td>2.81</td><td>34.55</td></tr>
+<tr><td>Cysteine</td><td>tgt</td><td>1.29</td><td>39.37</td></tr>
+<tr><td>Leucine</td><td>tta</td><td>0.63</td><td>6.94</td></tr>
+<tr><td>Phenylananine</td><td>ttc</td><td>1.48</td><td>51.04</td></tr>
+<tr><td>Leucine</td><td>ttg</td><td>1.17</td><td>12.99</td></tr>
+<tr><td>Phenylananine</td><td>ttt</td><td>1.42</td><td>48.96</td></tr>
+</table>
+
+__EOF
 #########################################################################################
 #				Restriction Enzymes 					#
 # The 5', middle section and 3' regions of the sequence are separated using the 	#
@@ -203,7 +287,6 @@ __EOF
 #########################################################################################
 
 print <<__EOF;
-</table>
 </div>
 <br />
 <h2>Restriction Enzymes: </h2>
